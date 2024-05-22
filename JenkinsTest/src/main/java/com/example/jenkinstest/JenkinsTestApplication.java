f67e14a5fb30412a72511e0a4d79338ecb6385e1
Keye Li <keyeli@Keyes-MacBook-Pro.local>
2024-05-21 20:42:54 -0700

    created new project

---
+++ JenkinsTest/src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,19 @@
+package com.example.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+
+
+
+        SpringApplication.run(JenkinsTestApplication.class, args);
+
+
+        System.out.println("hello world");
+    }
+
+}
